be858bc10a258800f7242c712d8aac0c3bedce00
Eric Wang <ewang1@mit.edu>
2022-01-14 21:09:07 -0500

    added mongodb stuff

--- server/server.js
+++ server/server.js
@@ -25,6 +25,9 @@ const session = require("express-session"); // library that stores info about ea
 const mongoose = require("mongoose"); // library to connect to MongoDB
 const path = require("path"); // provide utilities for working with file and directory paths
 
+// use .env
+const dotenv = require("dotenv").config();
+
 const api = require("./api");
 const auth = require("./auth");
 
@@ -33,9 +36,9 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = process.env.ATLAS_SRV;
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "testingDB1";
 
 // connect to mongodb
 mongoose
