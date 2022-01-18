aad6c377d2a482e6fb17bdf1ef2ddc84334cf09b
Saketh Gabbita <saketh@dhcp-10-29-79-7.dyn.mit.edu>
2022-01-18 17:18:41 -0500

    basic rules no formatting

---
+++ client/src/components/pages/HowToPlay.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+import "../../utilities.css";
+
+const HowToPlay = () => {
+  return (
+    <div>
+      <p>
+        Welcome to Minion Battle. In this game you will face another player and try to destroy their
+        towers. To do so, collect and use gold to buy and deploy minions from one of your towers.
+        Once all of your towers lose all health, the game is over.
+      </p>
+    </div>
+  );
+};
+
+export default HowToPlay;
