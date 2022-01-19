9823e9a8a7bbc0db9280babe8e080d70411877ac
Saketh Gabbita <saketh@dhcp-10-29-79-7.dyn.mit.edu>
2022-01-19 12:57:36 -0500

    working on goldmines

--- client/src/components/pages/CreateMap.tsx
+++ client/src/components/pages/CreateMap.tsx
@@ -18,7 +18,6 @@ type CreateMapProps = RouteComponentProps & {
 };
 
 const CreateMap = (props: CreateMapProps) => {
-  const [addGoldToggled, setAddGoldToggled] = useState<boolean>(false);
   const [mapName, setMapName] = useState<string>("");
   const [creatorName, setCreatorName] = useState<string>(props.userId);
   const [numPlayers, setNumPlayers] = useState<number>(2);
@@ -29,22 +28,28 @@ const CreateMap = (props: CreateMapProps) => {
 
   const canvasWidth = 1600 / scaleFactor;
   const canvasHeight = 750 / scaleFactor;
+  let canvas;
 
   useEffect(() => {
-    const canvas = document.getElementById("create-canvas") ?? assert.fail();
+    canvas = document.getElementById("create-canvas") ?? assert.fail();
     drawCreateCanvas();
     console.log("init");
-    canvas.addEventListener("click", (event: MouseEvent) => {
-      console.log("click");
-      let drawCoord: Point = { x: event.offsetX, y: event.offsetY };
-      drawGoldMine(drawCoord);
-      let officialCoord: Point = { x: scaleFactor * event.offsetX, y: scaleFactor * event.offsetY };
-    });
   }, []);
 
-  const toggleAddGold = () => {
-    setAddGoldToggled(!addGoldToggled);
-  };
+  useEffect(() => {
+    canvas = document.getElementById("create-canvas") ?? assert.fail();
+    const handleClick = (event: MouseEvent) => {
+      console.log("click");
+      let drawCoord: Point = { x: event.offsetX, y: event.offsetY };
+      console.log(drawCoord.x + " " + drawCoord.y);
+      drawGoldMine(drawCoord);
+      let officialCoord: Point = { x: scaleFactor * event.offsetX, y: scaleFactor * event.offsetY };
+      setGoldMines([...goldMines, officialCoord]);
+      console.log(goldMines.length);
+    };
+    canvas.addEventListener("click", handleClick);
+    return canvas.removeEventListener("click", handleClick);
+  }, [goldMines]);
 
   const handleCreatorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCreatorName(event.target.value);
@@ -58,6 +63,7 @@ const CreateMap = (props: CreateMapProps) => {
     const mapInfo = {
       name: mapName,
       creator_id: props.userId,
+      creator_name: creatorName,
       num_players: numPlayers,
       gold_mines: goldMines,
       towers: towers,
@@ -77,9 +83,7 @@ const CreateMap = (props: CreateMapProps) => {
         <div className="u-flex">
           <div className="Creation-subContainer">
             <canvas id="create-canvas" width={canvasWidth} height={canvasHeight} />
-            <button className="Creation-button u-pointer" onClick={toggleAddGold}>
-              Add gold
-            </button>
+            <div className="Creation-button u-pointer">Add gold</div>
           </div>
           <div className="Creation-subContainer">
             <div>
@@ -104,14 +108,17 @@ const CreateMap = (props: CreateMapProps) => {
                 />
               </div>
               <div className="configurables">
-                <button
-                  type="submit"
-                  className="Creation-button u-pointer"
-                  value="Save Map"
-                  onClick={handleSaveMap}
-                >
-                  Save Map
-                </button>
+                <form>
+                  <button
+                    type="submit"
+                    className="Creation-button u-pointer"
+                    value="Save Map"
+                    onClick={handleSaveMap}
+                    formAction="/"
+                  >
+                    Save Map
+                  </button>
+                </form>
               </div>
             </div>
           </div>
