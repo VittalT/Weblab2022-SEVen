4297d0b2d08bcde3a922faa4f0585376dbb4f2df
Eric Wang <ewang1@mit.edu>
2022-01-20 21:05:16 -0500

    lot of changes

--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,10 @@ import path from "path"; // Allows us to retrieve file paths
 import auth from "./auth"; // weblab authentication helper
 import socketManager from "./server-socket"; // websockets
 import api from "./api";
+
+// cookies
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config();
 // Loads environmental variables
 dotenv.config({});
@@ -39,6 +43,7 @@ const sessionSecret = process.env.SESSION_SECRET;
 if (sessionSecret === undefined) {
   throw new Error("Please add a session secret as 'SESSION_SECRET'");
 }
+
 app.use(
   session({
     secret: sessionSecret,
@@ -46,6 +51,9 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+//cookie parser
+app.use(cookieParser());
 app.use(auth.populateCurrentUser);
 app.use("/api", api);
 
