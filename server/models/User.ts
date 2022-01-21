68ac115e20a6113d4aee5778a76e77b4bbc415dd
Saketh Gabbita <sakethgabbita@gmail.com>
2022-01-20 21:09:03 -0500

    added leaderboard fields to User

--- server/models/User.ts
+++ server/models/User.ts
@@ -4,12 +4,20 @@ const UserSchema = new Schema({
   name: String,
   googleid: String,
   current_game: String,
+  rating: Number,
+  all_time_rating: Number,
+  games_played: Number,
+  games_won: Number
 });
 
 export interface User extends Document {
   name: string;
   googleid: string;
   current_game: string;
+  rating: number;
+  all_time_rating: number;
+  games_played: number;
+  games_won: number;
   _id: string;
 }
 
