ce8494de0a4f514284a89e01ef1e9fafa6d64f04
Vittal Thirumalai <vittalt@mit.edu>
2022-01-22 20:23:28 -0500

    Current

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -1,44 +1,41 @@
-declare global {
-  enum ClickState {
-    Tower = "Tower",
-    Minion = "Minion",
-    MinionFirstTower = "Minion and Clicked First Tower",
-    Explosion = "Explosion",
-  }
-
-  enum Size {
-    Small = "Small",
-    Medium = "Medium",
-    Large = "Large",
-  }
-
-  type TowerConstants = {
-    health: number;
-    healthRegenRate: number;
-    goldRate: number;
-    cost: number;
-    minAdjBuildRadius: number;
-    maxAdjBuildRadius: number;
-    hitRadius: number;
-  };
-
-  type MinionConstants = {
-    damageRate: number;
-    cost: number;
-    speed: number;
-    boundingRadius: number;
-  };
-
-  type GameUpdateData = {
-    time: number;
-    hostId: string;
-    idToName: Record<string, string>;
-    playerToTeamId: Record<string, number>;
-    activeStatus: string;
-    winnerId: string | null;
-    players: Record<string, Player>;
-    towers: Record<number, Tower>;
-    minions: Record<number, Minion>;
-  };
+enum ClickState {
+  Tower = "Tower",
+  Minion = "Minion",
+  MinionFirstTower = "Minion and Clicked First Tower",
+  Explosion = "Explosion",
 }
-export {};
+
+enum Size {
+  Small = "Small",
+  Medium = "Medium",
+  Large = "Large",
+}
+
+type TowerConstants = {
+  health: number;
+  healthRegenRate: number;
+  goldRate: number;
+  cost: number;
+  minAdjBuildRadius: number;
+  maxAdjBuildRadius: number;
+  hitRadius: number;
+};
+
+type MinionConstants = {
+  damageRate: number;
+  cost: number;
+  speed: number;
+  boundingRadius: number;
+};
+
+type GameUpdateData = {
+  time: number;
+  hostId: string;
+  idToName: Record<string, string>;
+  playerToTeamId: Record<string, number>;
+  activeStatus: string;
+  winnerId: string | null;
+  players: Record<string, Player>;
+  towers: Record<number, Tower>;
+  minions: Record<number, Minion>;
+};
