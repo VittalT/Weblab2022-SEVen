30e3be066aa05bbe2078eedf53f40a077bf4f517
Eric Wang <ewang1@mit.edu>
2022-01-25 17:54:14 -0500

    made lobby pretty

--- client/src/components/pages/Lobby.tsx
+++ client/src/components/pages/Lobby.tsx
@@ -14,6 +14,8 @@ import LobbyGameDisplay from "../modules/LobbyGameDisplay";
 import { Game } from "./FindGame";
 import { socket } from "../../client-socket";
 
+import Button from "@mui/material/Button";
+
 type Props = RouteComponentProps & {
   passedUserId: string;
   joinRoom: (userId: string, gameCode: string) => void;
@@ -48,14 +50,31 @@ const Lobby = (props: Props) => {
     <>
       <div className="Lobby-container">
         <h3 className="Lobby-header">MINION BATTLE</h3>
-        <div>LOBBY</div>
-        <div>{publicGames.length + " Games"}</div>
-        <div>
-          {publicGames.map((game: { hostName: string; gameCode: string }) => (
-            <button onClick={() => joinPublicGame(game.gameCode)}>
-              {game.hostName + " " + game.gameCode}
-            </button>
-          ))}{" "}
+        <div className="Lobby-lobbyContainer">
+          <div className="Lobby-lobbyTitle">PUBLIC GAMES</div>
+          <div>
+            {publicGames.map((game: { hostName: string; gameCode: string }) => (
+              <div>
+                <Button
+                  size="medium"
+                  sx={{
+                    marginTop: 1,
+                    marginBottom: 1,
+                    fontSize: 15,
+                    borderRadius: 3,
+                    backgroundColor: "#98c1d9",
+                    "&:hover": {
+                      backgroundColor: "#6CB1D9",
+                    },
+                  }}
+                  variant="contained"
+                  onClick={() => joinPublicGame(game.gameCode)}
+                >
+                  {game.hostName + "'s Game"}
+                </Button>
+              </div>
+            ))}{" "}
+          </div>
         </div>
       </div>
       <BackButton text="BACK" destPath="/findgame" />
