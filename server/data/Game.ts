9a0f0ed439f2b805197ad451d7f6a19902d0722e
Eric Wang <ewang1@mit.edu>
2022-01-25 01:19:56 -0500

    fixed game ending bug

--- server/data/Game.ts
+++ server/data/Game.ts
@@ -438,6 +438,9 @@ export class Game {
   public checkWin() {
     let remainingPlayers = 0;
     let remainingPlayerId = "";
+    if (Date.now() - this.startTime > 10 * 60 * 1000) {
+      this.winnerId = this.playerIds[0];
+    }
     for (const playerId of Object.keys(this.players)) {
       const player = this.players[playerId];
       if (player.towerIds.length === 0) {
@@ -456,9 +459,6 @@ export class Game {
 
   public onGameEnd(): void {
     this.isInPlay = false;
-    if (Date.now() - this.startTime > 10 * 60 * 1000) {
-      this.winnerId = this.playerIds[0];
-    }
     if (this.winnerId !== null) {
       const winnerName = this.idToName[this.winnerId];
       endGame(this.gameCode, winnerName);
